0fedb7f7dffb6ac0c2e10da9e6c319c427c0c61e
Tomat <xxlennygamerxx@gmail.com>
2023-05-23 22:34:18 -0700

    god

--- src/Tomat.Tetron.Avalonia/App.axaml.cs
+++ src/Tomat.Tetron.Avalonia/App.axaml.cs
@@ -1,17 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Beutl;
 using Beutl.Configuration;
 using Beutl.Media;
 using FluentAvalonia.Core;
+using FluentAvalonia.Styling;
 
 namespace Tomat.Tetron.Avalonia;
 
 public sealed partial class App : Application {
     private MainViewModel mainViewModel;
-    
+
     public override void Initialize() {
         FAUISettings.SetAnimationsEnabledAtAppLevel(true);
 
@@ -21,8 +26,37 @@ public sealed partial class App : Application {
 
         var config = GlobalConfiguration.Instance;
         config.Restore(GlobalConfiguration.DefaultFilePath);
+        var view = config.ViewConfig;
+        CultureInfo.CurrentUICulture = view.UICulture;
 
         AvaloniaXamlLoader.Load(this);
+        Resources["PaletteColors"] = colors;
+
+        view.GetObservable(ViewConfig.ThemeProperty)
+            .Subscribe(x => {
+                Dispatcher.UIThread.InvokeAsync(() => {
+                    FluentAvaloniaTheme thm = AvaloniaLocator.Current.GetService<FluentAvaloniaTheme>()!;
+
+                    switch (x) {
+                        case ViewConfig.ViewTheme.Light:
+                            thm.RequestedTheme = FluentAvaloniaTheme.LightModeString;
+                            break;
+
+                        case ViewConfig.ViewTheme.Dark:
+                            thm.RequestedTheme = FluentAvaloniaTheme.DarkModeString;
+                            break;
+
+                        case ViewConfig.ViewTheme.HighContrast:
+                            thm.RequestedTheme = FluentAvaloniaTheme.HighContrastModeString;
+                            break;
+
+                        case ViewConfig.ViewTheme.System:
+                            thm.PreferSystemTheme = true;
+                            thm.InvalidateThemingFromSystemThemeChanged();
+                            break;
+                    }
+                });
+            });
     }
 
     public override void OnFrameworkInitializationCompleted() {
